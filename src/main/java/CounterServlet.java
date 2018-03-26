19af604acf9645651ce481cd140eb14388dd2e87
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 15:56:33 -0500

    simpilified the doGet to only use response.getWriter

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -4,21 +4,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
     private int hitCount;
 
     public void init() {
-        // Reset hit counter.
+        // reset hit counter
         hitCount = 0;
     }
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Counter!</h1>");
+        response.getWriter().println("<h1>Counter!</h1>");
         hitCount++;
-        out.println("<h2>Total Number of hits: " + hitCount + "</h2>");
+        response.getWriter().println("<h2>Total Number of hits: " + hitCount + "</h2>");
     }
 }
