6874809265fc5bb7b64c5d23ed6f445e1b20a531
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 15:07:24 -0500

    added counter servlet class to exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet {
+    private int hitCount;
+
+    public void init() {
+        // Reset hit counter.
+        hitCount = 0;
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Counter!</h1>");
+        hitCount++;
+        out.println("<h2>Total Number of hits: " + hitCount + "</h2>");
+    }
+}
