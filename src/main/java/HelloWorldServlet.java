671ac7cd1f86eeb6f00654cc31bf8504ed565ca9
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 15:55:49 -0500

    removed out lines so that the file just uses response.getWriter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,17 +6,14 @@ import javax.servlet.http.*;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
         if(name == null) {
             name = "World";
         }
-        out.println("<h1 style=\"font-family: fantasy;\">Hello, " + name + "!</h1>");
+        response.getWriter().println("<h1 style=\"font-family: fantasy;\">Hello, " + name + "!</h1>");
         response.getWriter().println("<p> Enter Your Name:</p>");
         response.getWriter().println("<form>");
         response.getWriter().println("<input name =\" name\" />");
         response.getWriter().println("<form>");
-
     }
 }
