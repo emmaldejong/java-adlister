abba690882c92aa3e29595bc738bd6ff745da493
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 16:54:09 -0500

    started refactor to take user reset input

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -8,15 +8,20 @@ import java.io.IOException;
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
     private int hitCount;
+    private boolean reset;
 
     public void init() {
         // reset hit counter
         hitCount = 0;
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("<h1>Counter!</h1>");
         hitCount++;
         response.getWriter().println("<h2>Total Number of hits: " + hitCount + "</h2>");
+        if(reset == true) {
+            init();
+        }
     }
 }
