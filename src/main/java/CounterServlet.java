4f5a7259a93c5bd3fcbd1566c42a06e98e671a12
emmadejong <dejong.l.emma@gmail.com>
2018-03-27 09:58:28 -0500

    added reset button code to page but behavior is not right

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -8,20 +8,23 @@ import java.io.IOException;
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
     private int hitCount;
-    private boolean reset;
 
     public void init() {
         // reset hit counter
         hitCount = 0;
     }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("<h1>Counter!</h1>");
-        hitCount++;
         response.getWriter().println("<h2>Total Number of hits: " + hitCount + "</h2>");
-        if(reset == true) {
-            init();
+        String reset = request.getParameter("reset");
+        if (reset != null && reset.equalsIgnoreCase("reset")) {
+            hitCount = 0;
+        } else {
+            hitCount++;
         }
+        response.getWriter().println("<form>");
+        response.getWriter().println("<input name = 'reset' value='Reset' type='submit'>");
+        response.getWriter().println("</form>");
     }
 }
