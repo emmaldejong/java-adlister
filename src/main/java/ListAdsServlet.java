4aa3af624bc7e7b2ef4081c577f7a07a89820daf
emmadejong <dejong.l.emma@gmail.com>
2018-03-28 14:36:08 -0500

    added setAttribute to ads and List to DaoFactory

--- src/main/java/ListAdsServlet.java
+++ src/main/java/ListAdsServlet.java
@@ -4,15 +4,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ListAdsServlet", urlPatterns = "/ads")
 public class ListAdsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/ads/index.jsp");
-//    }
-
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.getRequestDispatcher("/ads/index.jsp");
-//    }
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+        request.setAttribute("ads", ads);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
