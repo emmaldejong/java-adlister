6eef0884ed0838bba738b9fe133de6b20d1172e5
emmadejong <dejong.l.emma@gmail.com>
2018-03-28 12:13:12 -0500

    added walkthrough solution notes

--- src/main/java/PickColorServlet.java
+++ src/main/java/PickColorServlet.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 public class PickColorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String favColor = request.getParameter("favColor");
+        //grabbing the value from the color form
         request.setAttribute("favColor", favColor);
         request.getRequestDispatcher("/redirectColor.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //if they go to /pickcolor, here we are sending them to the form .jsp where they can do so
         request.getRequestDispatcher("/enterColor.jsp").forward(request, response);
     }
 }
