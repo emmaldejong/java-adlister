0e128b3cdbd05635a27aa14a5e039410c53b8621
emmadejong <dejong.l.emma@gmail.com>
2018-03-28 14:12:36 -0500

    redirects and logic working in new servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
