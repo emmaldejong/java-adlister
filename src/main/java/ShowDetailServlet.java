a742a0ca6abbcc5ada2a04cb737305f67f777ffd
emmadejong <dejong.l.emma@gmail.com>
2018-03-28 16:19:54 -0500

    created servlets to see ad details

--- src/main/java/ShowDetailServlet.java
+++ src/main/java/ShowDetailServlet.java
@@ -1,10 +1,19 @@
-@javax.servlet.annotation.WebServlet(name = "ShowDetailServlet")
-public class ShowDetailServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "ShowDetailServlet", urlPatterns = "/show")
+public class ShowDetailServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/show.jsp").forward(request, response);
     }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String adId = request.getParameter("ad");
+        request.setAttribute("ad", adId);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
