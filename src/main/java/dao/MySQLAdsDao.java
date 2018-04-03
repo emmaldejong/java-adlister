1b188d614ba3e8a1a34e89b46181bbd6f5b47966
emmadejong <dejong.l.emma@gmail.com>
2018-04-03 11:08:31 -0500

    added method overides and connection

--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -2,12 +2,37 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.mysql.cj.jdbc.Driver;
+import models.Ad;
 
 public class MySQLAdsDao extends Ads {
-    DriverManager.registerDriver(new Driver());
-    Connection connection = DriverManager.getConnection(
-            "jdbc:mysql://localhost/adlister_db",
-            "admin",
-            "codeup"
-    );
+    private static Connection connection = null;
+
+    public static void main(String[] args) {
+
+
+        try {
+            DriverManager.registerDriver(new Driver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+    }
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
 }
