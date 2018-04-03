1495c73db9fc7cb4e19ec1bb9cccfbc7267009af
emmadejong <dejong.l.emma@gmail.com>
2018-04-03 10:44:26 -0500

    create db and tables

---
+++ migration.sql
@@ -0,0 +1,21 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50),
+  password VARCHAR(50),
+  email VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
+  title VARCHAR(200),
+  description VARCHAR(400),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  PRIMARY KEY (id)
+);
