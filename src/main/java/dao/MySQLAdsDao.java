53b12ab4c2fc296f00af702bb2dfe964808f8486
emmadejong <dejong.l.emma@gmail.com>
2018-04-03 11:41:16 -0500

    added try catch

--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -1,8 +1,6 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 import com.mysql.cj.jdbc.Driver;
