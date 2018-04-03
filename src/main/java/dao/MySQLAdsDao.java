fce5765565fa8213f4efd790b571c0cb47088531
emmadejong <dejong.l.emma@gmail.com>
2018-04-03 10:58:03 -0500

    added connection class to project

--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -1,4 +1,13 @@
 package dao;
 
-public class MySQLAdsDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLAdsDao extends Ads {
+    DriverManager.registerDriver(new Driver());
+    Connection connection = DriverManager.getConnection(
+            "jdbc:mysql://localhost/adlister_db",
+            "admin",
+            "codeup"
+    );
 }
