0708dfdc07b00aa3ebacb9b175219f6186498a6b
emmadejong <dejong.l.emma@gmail.com>
2018-04-04 20:08:16 -0500

    added users dao to facotry

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -5,11 +5,19 @@ import com.codeup.adlister.Config;
 public class DaoFactory {
     private static Ads adsDao;
     private static Config config = new Config();
+    private static Users usersdao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+
+    public static Users getUsersdao() {
+        if (usersdao == null) {
+            usersdao = new MySQLUsersDao(config);
+        }
+        return usersdao;
     }
 }
