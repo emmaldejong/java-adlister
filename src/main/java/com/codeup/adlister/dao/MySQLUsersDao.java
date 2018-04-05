b29e3e185cbef70e4c84369babc4e7bcfd7fdfa1
emmadejong <dejong.l.emma@gmail.com>
2018-04-04 20:10:13 -0500

    created new users dao file

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,4 +1,66 @@
 package com.codeup.adlister.dao;
 
-public class MySQLUsersDao {
+
+import com.codeup.adlister.Config;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users{
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        String query = "SELECT * FROM USERS WHERE username = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1,username);
+            ps.execute();
+            ps.getResultSet();
+            ResultSet rs = ps.getResultSet();
+            if(rs.next()){
+                return new User(
+                        rs.getLong("id"),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password")
+                );
+            } else return null;
+        } catch (SQLException e) {
+            throw new RuntimeException("user already exists", e);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        String newUser = "INSERT INTO users(username,email,password) VALUES (?,?,?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(newUser, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error registering account.", e);
+        }
+    }
 }
